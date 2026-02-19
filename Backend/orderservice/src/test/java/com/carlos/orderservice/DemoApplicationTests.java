21c77b3e0f5e99ffb4c9536260074debdede877b
Carlos Lopez <carlosgablopezm@gmail.com>
2026-02-19 18:08:11 -0500

    new:projects initialized

---
+++ Backend/orderservice/src/test/java/com/carlos/orderservice/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carlos.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
